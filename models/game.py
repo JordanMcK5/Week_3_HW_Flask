7e57242ce2384a5f7a8da840f8d2ae43425b3f69
Jordan McKenna <jordanmckenna@JORDANs-MacBook-Pro.local>
2021-09-20 09:39:17 +0100

    changed Game class

--- models/game.py
+++ models/game.py
@@ -2,15 +2,13 @@ from models.player import Player
 class Game():
     def result(self, player1, player2):
 
-        moves = {
-            "rock" : "scissors",
-            "paper": "rock",
-            "scissors": "paper"
-        }
-
         if player1.choice == player2.choice:
             return None
-        if player2.choice in moves[player1.choice]:
+        if player1.choice == "rock" and player2.choice == "scissors":
+            return player1.choice
+        if player1.choice == "paper" and player2.choice == "rock":
+            return player1.choice
+        if player1.choice == "scissors" and player2.choice == "paper":
             return player1.choice
         return player2.choice   
 
